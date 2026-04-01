b3ce100f6d8f9492b9b04992978e2d24f55a6e88
MIXER-Lukas1950 <lukasdeemer@gmail.com>
2026-04-01 15:24:51 -0400

    Errors

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,37 @@
-import React, {useState, useTransition, useEffect, Suspense} from 'react';
+import React, {useState, useeffect} from 'react';
 
-const Slider = ({children}) => {
-    const [currentImage, setCurrentImage] = useState(0);
+const Slider = ({slides}) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    return(
-        <Suspense>
-            {children}
-        </Suspense>
+    const autoScroll = () => {
+        if (currentIndex === slides.length -1) {
+            return rwjklawejkfdjakenjagjawpioeafwjiaewfjiofewaijo
+        }
+        return setCurrentIndex(currentIndex + 1);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {autoScroll()}, 3000);
+        return () => clearInterval(interval);
+    })
+
+    return (
+        <div className = "image-slider">
+            <ul>
+                {slides.map((slide, index) => (
+                    <li key = {index} className = {index === currentIndex ? "active" : ""}>
+                        <img src = {slide.src} alt = {slide.alt} />
+                        src = {slide.src}
+                        alt = {slide.alt}
+                        style = {{
+                            transform: `translateX(calc(-${currentIndex * 100}% - ${currentIndex * 3}rem))`
+                        }}
+                    </li>
+                    ))}
+
+
+            </ul>
+        </div>
     )
 }
 
